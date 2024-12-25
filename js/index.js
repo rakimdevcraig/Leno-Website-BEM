a6f928fee3e514acf63702435ebb2ed4a9d7244d
Rakim <rakimdevcraig@gmail.com>
2024-12-25 10:14:15 -0500

    added navbar scroll and started testimonials section

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenu.classList.toggle("active");
   });
 });
+
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar--scroll");
+  } else {
+    navbar.classList.remove("navbar--scroll");
+  }
+});
