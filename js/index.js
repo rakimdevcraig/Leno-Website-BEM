b9a5d08bba1850e53d04573cb89121e6f252917f
Rakim <rakimdevcraig@gmail.com>
2025-01-10 15:42:43 -0500

    modal done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //Mobile Menu
   let toggleButton = document.querySelector(".navbar__mobile-menu-toggle");
   let mobileMenu = document.querySelector(".navbar__mobile-menu-items");
 
   toggleButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+
+  //video modal
+  const modal = document.querySelector(".modal");
+  const videoButton = document.querySelector(".preview__video-button");
+  const closeButton = document.querySelector(".modal__close-button");
+  const videoPlayer = document.querySelector("#videoPlayer");
+  //Open modal when clicked
+  videoButton.addEventListener("click", function () {
+    //show modal modal
+    modal.style.display = "block";
+
+    //add src to video
+    videoPlayer.src =
+      "https://www.youtube.com/embed/XW1aGkzyjQg?si=yyMqoDLHN_1fAiJ";
+  });
+
+  //close modal on close button click
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+    videoPlayer.src = "";
+  });
+
+  //close modal on outer click
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      videoPlayer.src = "";
+    }
+  });
 });
 
+//Navigation Background on scroll
 window.addEventListener("scroll", function () {
   let navbar = document.querySelector(".navbar");
 
